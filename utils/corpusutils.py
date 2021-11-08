9026f25d159144bc8bdc4db6672c4d1504c9e4e9
Thomas Migliasso <thomas.migliasso@student-cs.fr>
2021-11-08 17:08:10 +0100

    add test for  load_tweet_with_annotation

--- utils/corpusutils.py
+++ utils/corpusutils.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pandas as pd
+from pytest import *
+
 
 datadir = '../Data/'
 annodir = datadir+'/tweets-annotations/'
 tweetdir = datadir+'/tweets/'
+
 
 def read_and_load_ann_annotation(filename):
 
@@ -20,11 +23,18 @@ def load_tweet_with_annotation(id):
         return(res)
 
 
+def test_load_tweet_with_annotation():
+    id = "143048389142134785"
+    dict = load_tweet_with_annotation(id)
+    assert dict = {'id': "143048389142134785", 'text': "Ce soir c'est l'élection de #missfrance et je vais me faire un plaisir de NE PAS regarder.", 'annotation': {'topics': [{'name': 'Languedoc', 'opinion': 'positive'}, {'name': 'Nord-Pas-De-Calais', 'opinicon': 'negative'}], 'negative_keywords': ['pas aime'], 'positive_keywords': ['jolie', 'aime']}}
+    id = "143049242305495040"
+    dict = load_tweet_with_annotation(id)
+    assert dict = "No annotations"
+
+
 def load_corpus_in_dataframe():
     ids = open(tweetdir+'tweets-ids').readlines()
     dict = {}
     for id in ids:
-        temp = load_tweet_with_annotation(id)
-        if temp != "No annotations":
-            dict[id] = temp
+        dict[id] = load_tweet_with_annotation(id)
     return pd.DataFrame.from_dict(dict)
