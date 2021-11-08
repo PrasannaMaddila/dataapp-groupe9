dc6307b37fb247a58b5a83068ed7158a7c64cf1a
Anouar Achghaf <anouar.achghaf@student-cs.fr>
2021-11-08 21:33:31 +0100

    merged code for corpusutils

--- utils/corpusutils.py
+++ utils/corpusutils.py
@@ -4,11 +4,149 @@ from pytest import *
 
 
 datadir = '../Data/'
-annodir = datadir+'/tweets-annotations/'
-tweetdir = datadir+'/tweets/'
+annodir = datadir+'tweets-annotations/part-0/'
+tweetdir = datadir+'tweets/'
 
 
-def read_and_load_ann_annotation(filename):
+from _pytest.mark import KeywordMatcher
+
+
+def read_and_load_ann_annotation(filename="143059118180139008.ann"):
+    # Reading the contents of the file
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        # This is the structure that we need, so
+        # we create an empty version.
+        parsed_dict = {
+            "topics": [],
+            "negative_keywords": [],
+            "positive_keywords": [],
+        }
+        # Processing the Title lines
+        # and creating a list of the words that
+        # we will use later.
+        word_list = {}
+        for line in lines:
+            if line[0] == "T":
+                if "Topic" in line:
+                    word_list[line[0:2]] = {
+                        "word": " ".join(line.split()[4:]),
+                        "type": "topics",
+                    }
+                elif "positive" in line.lower():
+                    word_list[line[0:2]] = {
+                        "word": line.split()[-1].lower(),
+                        "type": "positive",
+                    }
+                elif "negat" in line.lower():
+                    word_list[line[0:2]] = {
+                        "word": line.split()[-1].lower(),
+                        "type": "negative",
+                    }
+        # print(word_list)
+
+        # processing the Relation lines :
+        # Here, we create the actual list of parsed words,
+        # depending on if they're +ve, -ve etc.
+        key_word = ""
+        for line in lines:
+            # Ignore any Title lines - we've
+            # already gone through them
+            if line[0] != "R":
+                continue
+
+            # Getting each word.
+            words = line.split()
+
+            # Now, if its a positive Relation, use this section:
+            if "positive" in words[1].lower():
+                for word in words[2:]:
+                    key_word = word.partition(":")[2]
+                    if "topic" in word_list[key_word]["type"]:
+                        parsed_dict["topics"].append(
+                            {
+                                "name": word_list[key_word]["word"],
+                                "opinion": "positive",
+                            }
+                        )
+                    else:
+                        parsed_dict["positive_keywords"].append(
+                            word_list[key_word]["word"]
+                        )
+                    # end of if-insert-positive
+                # end of for
+            elif "Negates" in words[1]:
+                # Negative word insert for keyword "Negates".
+                # If the word is "Negates", concatenate all neg.words
+                # into one entry, and don't check for word type.
+                key_word = [word.partition(":")[2] for word in words[2:]]
+                negative_entries = []
+                for key in key_word:
+                    negative_entries.append(word_list[key]["word"])
+
+                parsed_dict["negative_keywords"].append(" ".join(negative_entries))
+            elif "Negative" in words[1]:
+                ## Here, we check the case of the word.
+                ## If word is TOPIC: insert into negative anyway.
+                ## Else: insert into "word_type"_keywords i.e. insert into
+                ## the type of the word
+                words = line.split()
+                for word in words[2:]:
+                    key_word = word.partition(":")[2]
+                    if "topics" in word_list[key_word]["type"]:
+                        parsed_dict["topics"].append(
+                            {
+                                "name": word_list[key_word]["word"],
+                                "opinion": "negative",
+                            }
+                        )
+                    else:
+                        # It is not a topic, and we need to check its type
+                        parsed_dict[word_list[key_word]["type"] + "_keywords"].append(
+                            word_list[key_word]["word"]
+                        )
+                    # end of if-insert-negative
+                # end of for """
+
+            # Checking to see what we've got
+        """ print("The Parsed Dictionary is: \n")
+        print(parsed_dict)
+        print("\n") """
+        # Reading for topic
+
+    return parsed_dict
+
+
+# Test for read_and_load():
+# from datavisualization.corpusutils import read_and_load_annotation
+from pytest import *
+
+# This is the test for our function above.
+def test_read_and_load_annotation():
+    # Given
+    filename = "143048389142134785.ann"
+    # When
+    annotations = read_and_load_annotation(filename)
+    # Then
+    assert annotations == {
+        "topics": [{"name": "élection de #missfrance", "opinion": "negative"}],
+        "negative_keywords": ["pas plaisir"],
+        "positive_keywords": ["plaisir"],
+    }
+    # Given
+    filename1 = "143059118180139008.ann"  # "143048389142134785.ann"  #
+    # When
+    annotations1 = read_and_load_annotation(filename1)
+    # Then
+    assert annotations1 == {
+        "topics": [
+            {"name": "Languedoc", "opinion": "positive"},
+            {"name": "Nord-Pas-De-Calais", "opinion": "negative"},
+        ],
+        "negative_keywords": ["pas aime"],
+        "positive_keywords": ["jolie", "aime"],
+    }
+
 
 
 def load_tweet_with_annotation(id):
@@ -33,10 +171,16 @@ def test_load_tweet_with_annotation():
 
 
 def load_corpus_in_dataframe():
-    ids = open(tweetdir+'tweets-ids').readlines()
+    ids = open(datadir+'tweets-ids').readlines()
     dict = {}
     for id in ids:
-        temp = load_tweet_with_annotation(id)
-        if temp != "No annotations":
-            dict[id] = temp
+        try:
+            temp = load_tweet_with_annotation(id[:-1])
+            if temp != "No annotations":
+                dict[id[:-1]] = temp
+        except:
+            continue
+        
     return pd.DataFrame.from_dict(dict)
+
+print(load_corpus_in_dataframe())
