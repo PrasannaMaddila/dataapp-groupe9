51d352be0694ab0ec0d6ddabd78457d7cbcd42ac
Thomas Migliasso <thomas.migliasso@student-cs.fr>
2021-11-08 16:45:51 +0100

    add load_tweet_with_annotation to corpusutils.py

--- utils/corpusutils.py
+++ utils/corpusutils.py
@@ -1,10 +1,20 @@
 import numpy as np
 import pandas as pd
 
+
 def read_and_load_ann_annotation(filename):
 
 
 def load_tweet_with_annotation(id):
+    S = read_and_load_ann_annotation(id+'.ann')
+    if S == {}:
+        return("No annotations")
+    else:
+        res = {}
+        res['id'] = id
+        res['text'] = open(id+'.txt').read()
+        res['annotations'] = S
+        return(res)
 
 
 def load_corpus_in_dataframe():
