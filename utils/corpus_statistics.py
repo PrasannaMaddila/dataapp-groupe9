7d1c32e0f46b1789e3299e1057869023486361ea
sharknaedo <prasanna.maddila@student-cs.fr>
2021-11-09 10:28:35 +0100

    utils: added subjects functions

--- utils/corpus_statistics.py
+++ utils/corpus_statistics.py
@@ -1,11 +1,9 @@
 # import numpy as np
 import pandas as pd
-<<<<<<< HEAD
-from ast import literal_eval
-=======
 import json
->>>>>>> 9596eef72d45131a1d865030c88351e3b197cf31
 from pytest import *
+
+# from corpusutils import corpus_dataframe
 
 """ my_string = "{'key':'val','key2':2}"
 my_dict = ast.literal_eval(my_string)
@@ -17,25 +15,40 @@ corpus_dataframe = pd.read_csv("./corpus_dataframe.csv")
 # Drops the automatically added index column
 corpus_dataframe = corpus_dataframe.iloc[:, 1:]
 # Re-converts the string dictionary to the dictionary-dictionary
-corpus_dataframe = corpus_dataframe.assign(
-    annotations=literal_eval(corpus_dataframe["annotations"])
-)
-print(corpus_dataframe["annotations"][:]["topics"])
+""" for ann in corpus_dataframe["annotations"]:
+    ann_dict = json.loads(ann.replace("'", '"'))
+print(type(corpus_dataframe["annotations"][0])) """
 
 
 def nb_tweets():
-    return len(open('../Data/tweets-ids').readlines())
+    return len(open("../Data/tweets-ids").readlines())
+
 
 def nb_annotations():
-    return len(corpus_dataframe['annotations'])
+    return len(corpus_dataframe["annotations"])
 
 
-def nb_subjects():
-    return len(corpus_dataframe["annotations"][:]["topics"])
+def nb_subjects(df):
+    # return [ann_dict["topics"] for ann_dict in len(corpus_dataframe["annotations"])
+    return len(subjects(df))
 
 
-def subjects():
-    return corpus_dataframe["annotations"][1:]["topics"]
+def subjects(df):
+    subs = []
+    for ann in df["annotations"]:
+        ann_dict = json.loads(ann.replace("'", '"'))
+        # print((ann_dict["topics"][0]["name"]))
+        # Case: Topics could be empty
+        try:
+            # If it works it works,
+            # Note: JSON returns a list of dictionary
+            # of size 1, i.e. it contains only our dict.
+            # Hence, the extra [0]
+            subs.append(ann_dict["topics"][0]["name"])
+        except:
+            # Else, Do nothing
+            continue
+    return subs
 
 
 def nb_positive_opinions():
@@ -53,11 +66,13 @@ def size_positive_vocab():
 # Need to update for using literal_eval'ed dataframe
 def size_negative_vocab():
     negative_keywords = set()
-    for ann in corpus_dataframe['annotations']:
+    for ann in corpus_dataframe["annotations"]:
         ann_dict = json.loads(ann.replace("'", '"'))
-        negative_keywords |= set(ann_dict['negative_keywords'])
+        negative_keywords |= set(ann_dict["negative_keywords"])
     print(negative_keywords)
     return len(negative_keywords)
 
+
 print(nb_tweets(), nb_annotations())
 print(size_negative_vocab())
+print(nb_subjects(corpus_dataframe), subjects(corpus_dataframe))
