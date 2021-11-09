1f622c4424688e020b77ba7197363ff2e5d71d39
Anouar Achghaf <anouar.achghaf@student-cs.fr>
2021-11-09 11:05:29 +0100

    test corpus_stats and fix subjects

--- utils/corpus_statistics.py
+++ utils/corpus_statistics.py
@@ -11,16 +11,16 @@ def nb_tweets():
     return len(open("../Data/tweets-ids").readlines())
 
 
-def nb_annotations():
-    return len(corpus_dataframe["annotations"])
+def nb_annotations(df=corpus_dataframe):
+    return len(df["annotations"])
 
 
-def nb_subjects(df):
+def nb_subjects(df=corpus_dataframe):
     # return [ann_dict["topics"] for ann_dict in len(corpus_dataframe["annotations"])
     return len(subjects(df))
 
 
-def subjects(df):
+def subjects(df=corpus_dataframe):
     subs = []
     for ann in df["annotations"]:
         ann_dict = json.loads(ann.replace("'", '"'))
@@ -31,16 +31,17 @@ def subjects(df):
             # Note: JSON returns a list of dictionary
             # of size 1, i.e. it contains only our dict.
             # Hence, the extra [0]
-            subs.append(ann_dict["topics"][0]["name"])
+            for topic in ann_dict["topics"]:
+                subs.append(topic["name"])
         except:
             # Else, Do nothing
             continue
     return subs
 
 
-def nb_positive_opinions():
+def nb_positive_opinions(df=corpus_dataframe):
     res = 0
-    for i in corpus_dataframe["annotations"]:
+    for i in df["annotations"]:
         dict_ann = json.loads(i.replace("'", '"'))
         topics = dict_ann["topics"]
         for k in topics:
@@ -49,9 +50,9 @@ def nb_positive_opinions():
     return res
 
 
-def nb_negative_opinions():
+def nb_negative_opinions(df=corpus_dataframe):
     res = 0
-    for i in corpus_dataframe["annotations"]:
+    for i in df["annotations"]:
         dict_ann = json.loads(i.replace("'", '"'))
         topics = dict_ann["topics"]
         for k in topics:
@@ -60,17 +61,26 @@ def nb_negative_opinions():
     return res
 
 
-def size_positive_vocab():
-    res = {}
-    for i in corpus_dataframe["annotations"]:
+def size_positive_vocab(df=corpus_dataframe):
+    res = set()
+    for i in df["annotations"]:
         dict_ann = json.loads(i.replace("'", '"'))
         res |= set(dict_ann["positive_keywords"])
-    return res
+    return len(res)
 
 
-def size_negative_vocab():
+def size_negative_vocab(df=corpus_dataframe):
     negative_keywords = set()
-    for ann in corpus_dataframe["annotations"]:
+    for ann in df["annotations"]:
         ann_dict = json.loads(ann.replace("'", '"'))
         negative_keywords |= set(ann_dict["negative_keywords"])
     return len(negative_keywords)
+
+# print(nb_tweets())
+# print(nb_annotations())
+# print(nb_negative_opinions())
+# print(nb_positive_opinions())
+print(nb_subjects())
+print(subjects())
+# print(size_positive_vocab())
+# print(size_negative_vocab())
