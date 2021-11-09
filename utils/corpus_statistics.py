eb5c2740bb1eb3565196d90456481c91152493b6
sharknaedo <prasanna.maddila@student-cs.fr>
2021-11-09 09:08:37 +0100

    Fixed reading from csv

--- utils/corpus_statistics.py
+++ utils/corpus_statistics.py
@@ -1,3 +1,7 @@
 # import numpy as np
 import pandas as pd
 from pytest import *
+
+corpus_dataframe = pd.read_csv("./corpus_dataframe.csv")
+corpus_dataframe = corpus_dataframe.iloc[:, 1:]
+print(corpus_dataframe)
