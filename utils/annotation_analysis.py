29188bdaaaebbe297e61e90f954a1cb269064839
Anouar Achghaf <anouar.achghaf@student-cs.fr>
2021-11-09 15:43:07 +0100

    first version of misses_opinions

--- utils/annotation_analysis.py
+++ utils/annotation_analysis.py
@@ -1,5 +1,29 @@
 import matplotlib.pyplot as plt
+import pandas as pd
+from difflib import *
+from pytest import *
+import json
 
+corpus_dataframe = pd.read_csv("./corpus_dataframe.csv")
+corpus_dataframe = corpus_dataframe.iloc[:, 1:]
+
+# returns a dictionnay
+# keys: Misses names, values : pair of (positive,negative)
+def misses_opinions(df=corpus_dataframe):
+    dict = {}
+    for ann in df['annotations']:
+        ann = json.loads(ann.replace("'", '"'))
+        for topic in ann['topics']:
+            words = topic['name'].lower().split()
+            if words[0] == 'miss':
+                if not topic['name'].upper() in dict:
+                    dict[topic['name'].upper()] = (0,0)
+                (a,b) = dict[topic['name'].upper()]
+                if topic['opinion'] == 'positive':
+                    a += 1
+                else: b += 1
+                dict[topic['name'].upper()] = (a,b)
+    return dict
 
 def pie_chart(df):
     raise NotImplementedError
@@ -11,3 +35,5 @@ def histogram_miss_tweet_number(df):
 
 def miss_pos_neg_data(df):
     raise NotImplementedError
+
+print(misses_opinions())
