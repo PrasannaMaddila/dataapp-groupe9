8d87cd42bc0a2a30e81fc02b9ad9f44a9ba9ca95
Anouar Achghaf <anouar.achghaf@student-cs.fr>
2021-11-09 16:01:50 +0100

    implement string matching for redundancies

--- utils/annotation_analysis.py
+++ utils/annotation_analysis.py
@@ -16,13 +16,22 @@ def misses_opinions(df=corpus_dataframe):
         for topic in ann['topics']:
             words = topic['name'].lower().split()
             if words[0] == 'miss':
-                if not topic['name'].upper() in dict:
-                    dict[topic['name'].upper()] = (0,0)
-                (a,b) = dict[topic['name'].upper()]
+                close_keys = get_close_matches(topic['name'].lower(), dict.keys(), 1, 0.75)
+                print(close_keys, topic['name'].lower())
+                if close_keys:
+                    close_keys = close_keys[0]
+                    if len(topic['name']) < len(close_keys):
+                        dict[topic['name'].upper()] = dict[close_keys]
+                        del dict[close_keys]
+                        close_keys = topic['name']
+                else: close_keys = topic['name'].lower()
+                if not close_keys in dict:
+                    dict[close_keys] = (0,0)
+                (a,b) = dict[close_keys]
                 if topic['opinion'] == 'positive':
                     a += 1
                 else: b += 1
-                dict[topic['name'].upper()] = (a,b)
+                dict[close_keys] = (a,b)
     return dict
 
 def pie_chart(df):
