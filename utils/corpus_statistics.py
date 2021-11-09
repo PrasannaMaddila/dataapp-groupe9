9596eef72d45131a1d865030c88351e3b197cf31
Anouar Achghaf <anouar.achghaf@student-cs.fr>
2021-11-09 09:48:37 +0100

    implement size_negative_vocab

--- utils/corpus_statistics.py
+++ utils/corpus_statistics.py
@@ -1,5 +1,6 @@
 # import numpy as np
 import pandas as pd
+import json
 from pytest import *
 
 corpus_dataframe = pd.read_csv("./corpus_dataframe.csv")
@@ -27,6 +28,12 @@ def size_positive_vocab():
     return 1
 
 def size_negative_vocab():
-    return 1
+    negative_keywords = set()
+    for ann in corpus_dataframe['annotations']:
+        ann_dict = json.loads(ann.replace("'", '"'))
+        negative_keywords |= set(ann_dict['negative_keywords'])
+    print(negative_keywords)
+    return len(negative_keywords)
 
 print(nb_tweets(), nb_annotations())
+print(size_negative_vocab())
