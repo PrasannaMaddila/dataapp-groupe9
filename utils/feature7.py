9b177d723cfb4a9b41b7271164738ed3c3705d44
Thomas Migliasso <thomas.migliasso@student-cs.fr>
2021-11-10 15:18:58 +0100

    adding interface (fixed)

--- utils/feature7.py
+++ utils/feature7.py
@@ -1,36 +1,31 @@
 import argparse
 from annotation_analysis import *
 
-corpus_dataframe = pd.read_csv("./corpus_dataframe.csv")
-corpus_dataframe = corpus_dataframe.iloc[:, 1:]
-
 dict_miss = misses_opinions()
 keys = dict_miss.keys()
 
-
 parser = argparse.ArgumentParser(
     description="Afficher les statistiques relatives à l'élection de la prochaine Miss France")
-parser.add_argument("--histogram", action="store_true",
+
+parser.add_argument("-H", "--histogram", action="store_true",
                     help="Display the histogram of all the competitors and their ratings in the Miss France competition")
-parser.add_argument("--pie_chart_opinions", action="store_true",
+parser.add_argument("-P", "--pie_chart_opinions", action="store_true",
                     help="Display the overall positivity of the competition")
-parser.add_argument("Topic", type=str,
+parser.add_argument("-T", "--Topic", type=str,
                     help="Display the information relative to a certain topic")
-parser.add_argument("Individual_histogram", type="store_true")
-parser.add_argument("Individual_pie_chart", type="store_true")
+parser.add_argument("-I", "--Individual_histogram", action='count', default=0)
+parser.add_argument("-Ip", "--Individual_pie_chart", action='count', default=0)
 
 args = parser.parse_args()
-
-print(keys)
 if args.Topic == 'Miss France':
-    print()
+    print("Information relative to the Miss France competion")
     if args.histogram:
         histogram(dict_miss)
-    if args.pie_chart_opinions:
+    elif args.pie_chart_opinions:
         pie_chart(corpus_dataframe)
 elif args.Topic in keys:
-    print("Choose the type of data you want")
-    if args.Individual_histogram:
-        individual_miss_graph_bar({args.Topic}, dict_miss)
-    if args.Individual_pie_chart:
-        individual_miss_graph_pie({args.Topic}, dict_miss)
+    print("Choose the type of data you want for a Miss")
+    if args.Individual_histogram == 1:
+        individual_miss_graph_bar(args.Topic, dict_miss)
+    if args.Individual_pie_chart == 1:
+        individual_miss_graph_pie(args.Topic, dict_miss)
