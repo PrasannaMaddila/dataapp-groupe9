4edceccd6bd1a4f565152ab48dda03317ee78d66
Thomas Migliasso <thomas.migliasso@student-cs.fr>
2021-11-10 09:24:31 +0100

    safety

--- utils/annotation_analysis.py
+++ utils/annotation_analysis.py
@@ -3,12 +3,17 @@ import pandas as pd
 from difflib import *
 from pytest import *
 import json
+from corpus_statistics import nb_negative_opinions, nb_positive_opinions
+import numpy as np
+
 
 corpus_dataframe = pd.read_csv("./corpus_dataframe.csv")
 corpus_dataframe = corpus_dataframe.iloc[:, 1:]
 
 # returns a dictionnay
 # keys: Misses names, values : pair of (positive,negative)
+
+
 def misses_opinions(df=corpus_dataframe):
     dict = {}
     for ann in df["annotations"]:
@@ -24,7 +29,8 @@ def misses_opinions(df=corpus_dataframe):
             topic["name"] = topic["name"].replace("-", " ")
             if words[0] == "miss":
                 close_keys = get_close_matches(
-                    topic["name"].lower()[5:], [k[5:] for k in dict.keys()], 1, 0.6
+                    topic["name"].lower()[5:], [k[5:]
+                                                for k in dict.keys()], 1, 0.6
                 )
                 if close_keys:
                     close_keys = "miss " + close_keys[0]
@@ -46,8 +52,46 @@ def misses_opinions(df=corpus_dataframe):
     return dict
 
 
-def pie_chart(df):
-    raise NotImplementedError
+def pie_chart(df=corpus_dataframe):
+    pos = nb_positive_opinions(df)
+    neg = nb_negative_opinions(df)
+    labels = 'Positive opinion proportion', 'Negative opinion proportion'
+    sizes = [pos, neg]
+    explode = (0.1, 0)
+    colors = ['g', 'r']
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels,
+            autopct='%1.1f%%', colors=colors, shadow=True, startangle=90, wedgeprops={'linewidth': 5})
+    ax1.axis('equal')
+    plt.show()
+
+
+# pie_chart()
+
+
+def histogram(df=corpus_dataframe):
+
+    test = {'Ms Jean-Marie Lafayette': (99, 3), 'Mr Jean Luc Mélenchon': (
+        50, 50), 'Ghandi the Wise': (98, 2)}
+    pos, neg = [], []
+    label = []
+    n = len(test)
+    ind = np.arange(n)
+    width = 0.5
+    for key in test.keys():
+        pos.append(test[key][0])
+        neg.append(test[key][1])
+        label.append(key)
+    p1 = plt.bar(ind, neg, width, color='r')
+    p2 = plt.bar(ind, pos, width, color='g')
+    plt.ylabel('Opinions')
+    plt.title('Opinions on Miss France')
+    plt.xticks(ind, label)
+    plt.legend((p1[0], p2[0]), ['Negative', 'Positive'])
+    plt.show()
+
+
+histogram()
 
 
 def histogram_miss_tweet_number(dict_miss):
