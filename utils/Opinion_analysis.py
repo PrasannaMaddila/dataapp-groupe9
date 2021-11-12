96b1b33d3ee6d6a47c98c69c9dce5b58a58aaca8
sharknaedo <prasanna.maddila@student-cs.fr>
2021-11-12 10:48:45 +0100

    added per miss opinion type fn

--- utils/Opinion_analysis.py
+++ utils/Opinion_analysis.py
@@ -9,18 +9,18 @@ corpus_dataframe = pd.read_csv("./utils/corpus_dataframe.csv", header=0)
 
 def opinion_of_event(df=corpus_dataframe):
     pos, neu, neg = 0, 0, 0
-    for tweet in df['text']:
+    for tweet in df["text"]:
         opinion = TextBlob(tweet).sentiment.polarity
-        if opinion > (1/3):
+        if opinion > (1 / 3):
             pos += 1
-        if opinion < -(1/3):
+        if opinion < -(1 / 3):
             neg += 1
         else:
             neu += 1
     nbr_tweets = pos + neu + neg
-    print("Percentage of positive tweets: {}%".format(pos*100/nbr_tweets))
-    print("Percentage of neutral tweets: {}%".format(neu*100/nbr_tweets))
-    print("Percentage of negative tweets: {}%".format(neg*100/nbr_tweets))
+    print("Percentage of positive tweets: {}%".format(pos * 100 / nbr_tweets))
+    print("Percentage of neutral tweets: {}%".format(neu * 100 / nbr_tweets))
+    print("Percentage of negative tweets: {}%".format(neg * 100 / nbr_tweets))
 
 
 def misses_tweets(df=corpus_dataframe):
@@ -41,8 +41,7 @@ def misses_tweets(df=corpus_dataframe):
                 continue
             if words[0] == "miss":
                 close_keys = get_close_matches(
-                    topic["name"].lower()[5:], [k[5:]
-                                                for k in dict.keys()], 1, 0.6
+                    topic["name"].lower()[5:], [k[5:] for k in dict.keys()], 1, 0.6
                 )
                 if close_keys:
                     close_keys = "miss " + close_keys[0]
@@ -68,18 +67,28 @@ def opinion_on_miss(miss, df=corpus_dataframe):
     tweets = dict_tweets[miss]
     pos, neu, neg = 0, 0, 0
     for line_nb in tweets:
-        tweet = df['text'][line_nb]
+        tweet = df["text"][line_nb]
         opinion = TextBlob(tweet).sentiment.polarity
-        if opinion > (1/3):
+        if opinion > (1 / 3):
             pos += 1
-        if opinion < -(1/3):
+        if opinion < -(1 / 3):
             neg += 1
         else:
             neu += 1
     nbr_tweets = pos + neu + neg
-    print("Percentage of positive tweets: {}%".format(pos*100/nbr_tweets))
-    print("Percentage of neutral tweets: {}%".format(neu*100/nbr_tweets))
-    print("Percentage of negative tweets: {}%".format(neg*100/nbr_tweets))
+    print("Percentage of positive tweets: {}%".format(pos * 100 / nbr_tweets))
+    print("Percentage of neutral tweets: {}%".format(neu * 100 / nbr_tweets))
+    print("Percentage of negative tweets: {}%".format(neg * 100 / nbr_tweets))
+    return (pos, neu, neg)
 
 
-opinion_on_miss('miss tahiti')
+def miss_type_opin(df=corpus_dataframe):
+    dict_misses_tweets = misses_tweets(df)
+    dict_type_opins = {}
+    for key in dict_misses_tweets.keys():
+        dict_type_opins[key] = opinion_on_miss(key, df)
+    return dict_type_opins
+
+
+opinion_on_miss("miss tahiti")
+print(miss_type_opin())
